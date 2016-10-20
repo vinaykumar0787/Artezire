9f97e7efa30b0cf3ba01e9c0e8ed69af00c1a94d
Vinay Kumar <vinay.kumar@merrillcorp.com>
2016-10-20 16:47:23 +0530

    Added OAuth Project

---
+++ src/Artezire.Web.OAuth/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.StaticFiles;
+using IdentityServer3.Core.Configuration;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Artezire.Web.OAuth
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            var certFile = env.WebRootPath + "\\idsrv3test.pfx";
+
+            var idsrvOptions = new IdentityServerOptions
+            {
+                Factory = new IdentityServerServiceFactory(),
+
+                SigningCertificate = new X509Certificate2(certFile, "idsrv3test"),
+                RequireSsl = false
+            };
+
+            app.UseIdentityServer(idsrvOptions);
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
--- src/Artezire/Controllers/ProductController.cs
+++ src/Artezire/Controllers/ProductController.cs
@@ -19,14 +19,14 @@ namespace Artezire.Controllers
 
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<string> Get(int id)
         {
             return "value";
         }
